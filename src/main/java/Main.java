9d5310fc45bbd1491e30c9fa180fce86e640d744
Jocelyn McHugo <97758428+plutonium-stan@users.noreply.github.com>
2023-02-14 15:14:35 -0700

    implement logging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,31 @@
 package main;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.time.Instant;
+
 public class Main {
+    public static String flName;
     public static void log(String text){
         System.out.println(text);
-        // TODO add file print implementation
+        try {
+            FileWriter writer = new FileWriter(flName);
+            writer.write(text);
+            writer.close();
+        } catch (IOException e){
+            System.out.println("File write failed");
+        }
     }
     public static void main(String[] args) {
+        flName = String.format("%s%sooad-fncd%slogs%sFNCD_%d.txt", Paths.get("").toAbsolutePath().toString(), File.separator, File.separator, File.separator, Instant.now().toEpochMilli());
+        File fl = new File(flName);
+        try {
+            fl.createNewFile();
+        } catch (IOException e) {
+            System.out.println("File creation failed.");
+        }
         log("Hello, world!");
     }
 }
