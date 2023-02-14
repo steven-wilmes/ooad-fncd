327959307e1dc133798a27f15afe416fc78f1003
Jocelyn McHugo <97758428+plutonium-stan@users.noreply.github.com>
2023-02-14 15:24:08 -0700

    create main function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import classes.Dealership;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -19,6 +21,7 @@ public class Main {
         }
     }
     public static void main(String[] args) {
+        // initialize logger
         flName = String.format("%s%sooad-fncd%slogs%sFNCD_%d.txt", Paths.get("").toAbsolutePath().toString(), File.separator, File.separator, File.separator, Instant.now().toEpochMilli());
         File fl = new File(flName);
         try {
@@ -26,6 +29,23 @@ public class Main {
         } catch (IOException e) {
             System.out.println("File creation failed.");
         }
-        log("Hello, world!");
+        
+        // initialize dealership
+        Dealership FNCD = new Dealership();
+        String[] days = new String[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        
+        // daily loop
+        for (int day=0; day < 30; day++){
+            log(String.format("It is %s", days[day%7]));
+            if (day%7==6){ // sunday
+                log("The FNCD is closed.");
+            }else{
+                FNCD.open();
+                FNCD.wash();
+                FNCD.repair();
+                FNCD.sell();
+                FNCD.end();
+            }
+        }
     }
 }
